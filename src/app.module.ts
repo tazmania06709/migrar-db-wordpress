f9480945ac9e3260624504efa1ed6a87b1b9332c
Josman Vicens Noa <tazmania06709>
2025-02-02 01:30:28 +0100

    update connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.SOURCE_DB_HOST,
+      host: 'localhost',
       port: 3306,
       username: process.env.SOURCE_DB_USER,
       password: process.env.SOURCE_DB_PASSWORD,
       database: process.env.SOURCE_DB_NAME,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: true,
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.WP_DB_HOST,
+      host: 'localhost',
       port: 3306,
-      username: process.env.WP_DB_USER,
-      password: process.env.WP_DB_PASSWORD,
+      username: process.env.SOURCE_DB_USER,
+      password: process.env.SOURCE_DB_PASSWORD,
       database: process.env.WP_DB_NAME,
       autoLoadEntities: true,
       synchronize: false,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
